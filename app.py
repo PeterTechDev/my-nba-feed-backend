bf3960cbfdb3d4abec4ed44ee24dde69bd671283
peterls <peterleite.dev@gmail.com>
2024-10-14 07:40:34 -0300

    trying to fix deploy on render

--- app.py
+++ app.py
@@ -1,31 +1,28 @@
+import os
 from flask import Flask, jsonify
 from tweety import Twitter
 from dotenv import load_dotenv
-import os
-
 from flask_cors import CORS
+
+# Load environment variables
+load_dotenv()
 
 app = Flask(__name__)
 CORS(app, resources={r"/*": {"origins": "https://petertechdev.github.io/my-nba-feed/"}})  # Replace with your frontend URL
 
-
-# Load environment variables from the .env file
-load_dotenv()
-
-
-# Get the Twitter session ID from the environment variable
 twitter_session_id = os.getenv("TWITTER_SESSION_ID")
 twitter_app = Twitter(twitter_session_id)
 
 @app.route('/api/tweets/<username>', methods=['GET'])
 def get_tweets(username):
     try:
-        # Get tweets from the user's account (e.g., 'celtics')
         all_tweets = twitter_app.get_tweets(username, pages=1)
-        tweet_ids = [tweet.id for tweet in all_tweets[:5]]  # Get the last 5 tweet IDs
+        tweet_ids = [tweet.id for tweet in all_tweets[:5]]
         return jsonify(tweet_ids)
     except Exception as e:
         return jsonify({"error": str(e)}), 500
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    # Use the PORT environment variable provided by Render
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
